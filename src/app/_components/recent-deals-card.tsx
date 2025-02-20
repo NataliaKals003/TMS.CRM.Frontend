11b3ec441a89741fa938215f420b75458cc1f0b2
Natalia Kals <nataliakals03@gmail.com>
2025-02-21 09:30:32 +1300

    making responsive recent deals card

--- src/app/_components/recent-deals-card.tsx
+++ src/app/_components/recent-deals-card.tsx
@@ -5,6 +5,7 @@ import { mockDeals, Deal } from '../types/deal';
 import { Card, CardContent, Typography, Box, Button } from '@mui/material';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import '../../styles/recent-deals-card-style.css';
 
 const RecentDealsCard = () => {
   const router = useRouter();
@@ -14,59 +15,30 @@ const RecentDealsCard = () => {
   };
 
   return (
-    <Card
-      sx={{
-        backgroundColor: '#FFFFFF',
-        color: '#092C4C',
-        position: 'relative',
-        border: '1px solid #EAEEF4',
-        boxShadow: 'none',
-        borderRadius: '12px',
-        flex: 1,
-        cursor: 'pointer',
-      }}
-    >
+    <Card className="cardRecentDeals">
       <CardContent>
         <Box display={'flex'} justifyContent="space-between" alignContent={'center'} marginBottom={3}>
-          <Typography variant="h5" component="div" style={{ fontWeight: 700, fontSize: 18 }}>
-            Recent Deals
-          </Typography>
-          <Button
-            onClick={handleDealClick}
-            variant="text"
-            color="primary"
-            sx={{
-              '&:hover': {
-                backgroundColor: 'transparent',
-                boxShadow: 'none',
-              },
-            }}
-          >
+          <Typography className="titleCard">Recent Deals</Typography>
+          <Button className="textButton" onClick={handleDealClick} variant="text" color="primary">
             View All
           </Button>
         </Box>
 
         {mockDeals.slice(0, 4).map((deal: Deal) => (
-          <Box onClick={() => router.push(`deal/${deal.id}`)} key={deal.id} display={'flex'} marginBottom={2}>
+          <Box className="dealBody" onClick={() => router.push(`deal/${deal.id}`)} key={deal.id}>
             <Image src={deal.dealPicture} alt="Deal" width={44} height={44} style={{ borderRadius: '50%' }} />
 
-            <Box display={'flex'} justifyContent={'space-between'} width={'100%'}>
-              <Box marginLeft={1.5} fontSize={14}>
-                <Typography variant="body2" color="092C4C" style={{ fontWeight: 700, fontSize: 16 }}>
+            <Box className="detailsBody">
+              <Box className="bodyText">
+                <Typography className="bodyTextBold" width={'100%'}>
                   {deal.street}
                 </Typography>
-                <Typography variant="body2" color="#7E92A2" style={{ fontWeight: 400, lineHeight: '27px' }}>
-                  {deal.city}
-                </Typography>
+                <Typography className="bodyTextBottom">{deal.city}</Typography>
               </Box>
 
-              <Box marginLeft={1.5} fontSize={14}>
-                <Typography variant="body2" color="092C4C" style={{ fontWeight: 700, fontSize: 16, textAlign: 'right' }}>
-                  {deal.price}
-                </Typography>
-                <Typography variant="body2" color="#7E92A2" width={'max-content'} textAlign={'right'} style={{ fontWeight: 400, lineHeight: '27px' }}>
-                  {deal.appointmentDate}
-                </Typography>
+              <Box className="bodyText">
+                <Typography className="bodyTextBold">{deal.price}</Typography>
+                <Typography className="bodyTextBottom">{deal.appointmentDate}</Typography>
               </Box>
             </Box>
           </Box>
